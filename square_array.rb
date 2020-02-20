db6e9877a1631a5133d6a2b266ee3ec5ec5b2624
kiran ram <kram@lions.molloy.edu>
2020-02-20 04:14:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,9 @@
 def square_array(array)
- new_array = []
-
- array.each do |integer|
-  new_array << integer ** 2
- end
- return new_array
+  new_array = []
+  array.each do |integer|
+    new_array << integest ** 2
+  end
+  return new_array
 end
 
 
